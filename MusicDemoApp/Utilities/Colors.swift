9d874f55fb1a136f1470677b41381f6fcbaa8ad4
Eren Erinanc <erenerinanc@gmail.com>
2021-10-07 13:54:49 +0300

    Add buttons

--- MusicDemoApp/Utilities/Colors.swift
+++ MusicDemoApp/Utilities/Colors.swift
@@ -10,7 +10,7 @@ import UIKit
 enum Colors {
     static let background = UIColor(red: 0.137, green: 0.063, blue: 0.243, alpha: 1.000)
     static let secondaryBackground = UIColor(red: 0.224, green: 0.137, blue: 0.353, alpha: 1.000)
-    static let primaryLabel = UIColor(red: 0.918, green: 0.224, blue: 0.780, alpha: 1.000)
+    static let primaryLabel = UIColor(red: 1.00, green: 0.26, blue: 0.84, alpha: 1.00)
     static let secondaryLabel = UIColor(red: 0.816, green: 0.686, blue: 0.890, alpha: 1.000)
 }
 
